57b0c12518ceeaf4ac0ef63cb65d556ed44534fd
Keyshawn Butts <kjbappsllc@gmail.com>
2022-08-18 18:04:16 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  prefix: 'tw-',
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography')
+  ]
+}
