10956303e3742702b32bea04e37813d34a302b6a
Keyshawn Butts <kjbappsllc@gmail.com>
2022-08-27 19:49:42 -0400

    updated some configuration

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,11 @@
 declare module '@tailwindcss/forms'
 declare module '@tailwindcss/aspect-ratio'
 declare module '@tailwindcss/typography'
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.jpg" {
+  const value: any;
+  export default value;
+}
