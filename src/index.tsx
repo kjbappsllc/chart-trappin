e033708331fb85259e8b421df75bd88d7d3910d5
Keyshawn Butts <kjbappsllc@gmail.com>
2022-08-27 20:03:08 -0400

    feat: udpated analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import { createRoot } from 'react-dom/client';
 import { AppRouter } from './app.routing';
+import { setupAnalytics } from './app.analytics'
 
 const container = document.getElementById('root');
 
 if (container != void 0) {
   const root = createRoot(container);
   root.render(<AppRouter />);
+  if (process.env.NODE_ENV === 'production') {
+    setupAnalytics()
+  }
 } else {
   console.error('Invalid root node for react to bind to');
 }
