18a1cf0be0dbedff322734e0fde9294aad45d002
Keyshawn Butts <kjbappsllc@gmail.com>
2022-10-15 14:13:03 -0400

    text change

--- src/routes/landing/components/courses.tsx
+++ src/routes/landing/components/courses.tsx
@@ -6,7 +6,7 @@ export const CoursesSection = ({ sectionId }: SectionProps) => {
       <div className="py-20 overflow-hidden radius-for-skewed">
         <div className="mt-8 mb-10 max-w-lg text-center mx-auto">
           <span className="text-primary-400 font-bold">Courses</span>
-          <h2 className="mt-4 text-4xl lg:text-5xl font-bold font-head">What can we teach you?</h2>
+          <h2 className="mt-4 text-4xl lg:text-5xl font-bold font-head">What we can teach you</h2>
         </div>
         <div className="bg-gray-50 grid grid-cols-2 gap-x-6 gap-y-4 py-4">
           <div className="w-full flex flex-col items-end pt-12 gap-y-8">
